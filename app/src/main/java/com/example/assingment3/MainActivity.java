9bbe34f4de7b43f24fa2f624f37c6542ee93100a
Tim Richards <timrichards90@gmail.com>
2023-10-18 20:27:35 -0600

    add floating action button and new ski areas

--- app/src/main/java/com/example/assingment3/MainActivity.java
+++ app/src/main/java/com/example/assingment3/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.assingment3;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +24,29 @@ public class MainActivity extends AppCompatActivity {
 
         skifieldsRecyclerView = findViewById(R.id.skifieldsRecyclerView);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.baseline_help_24); // Set the drawable to the FAB
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String snackbarText = "Tap a ski resort for its status, weather, and facility details.";
+                Snackbar.make(view, snackbarText, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
         List<Skifield> skifieldList = new ArrayList<>();
-        skifieldList.add(new Skifield("Broken River Ski Area", R.drawable.br_icon, "https://www.snow.nz/area/nz/canterbury/brokenriver/"));
-        skifieldList.add(new Skifield("Craigieburn Valley Ski Area", R.drawable.cv_icon, "https://www.snow.nz/area/nz/canterbury/craigieburn/"));
-        skifieldList.add(new Skifield("Hanmer Springs Ski Area", R.drawable.hs_icon, "https://www.snow.nz/area/nz/canterbury/hanmer-springs/"));
-        skifieldList.add(new Skifield("Mt Cheeseman Ski Area", R.drawable.mc_icon, "https://www.snow.nz/area/nz/canterbury/mt-cheeseman/"));
-        skifieldList.add(new Skifield("Mt Olympus Ski Area", R.drawable.mo_icon, "https://www.snow.nz/area/nz/canterbury/mt-olympus/"));
-        skifieldList.add(new Skifield("Temple Basin Ski Area", R.drawable.tb_icon, "https://www.snow.nz/area/nz/canterbury/temple-basin/"));
-        skifieldList.add(new Skifield("Rainbow Ski Area", R.drawable.rb_icon, "https://www.snow.nz/area/nz/canterbury/rainbow/"));
+        skifieldList.add(new Skifield("Broken River", R.drawable.br_icon, "https://www.snow.nz/area/nz/canterbury/brokenriver/"));
+        skifieldList.add(new Skifield("Craigieburn Valley", R.drawable.cv_icon, "https://www.snow.nz/area/nz/canterbury/craigieburn/"));
+        skifieldList.add(new Skifield("Fox Peak", R.drawable.fp_icon, "https://www.snow.nz/area/nz/mackenzie/foxpeak/"));
+        skifieldList.add(new Skifield("Hanmer Springs", R.drawable.hs_icon, "https://www.snow.nz/area/nz/canterbury/hanmer-springs/"));
+        skifieldList.add(new Skifield("Mt Cheeseman", R.drawable.mc_icon, "https://www.snow.nz/area/nz/canterbury/mt-cheeseman/"));
+        skifieldList.add(new Skifield("Mt Olympus", R.drawable.mo_icon, "https://www.snow.nz/area/nz/canterbury/mt-olympus/"));
+        skifieldList.add(new Skifield("Rainbow Valley", R.drawable.rb_icon, "https://www.snow.nz/area/nz/canterbury/rainbow/"));
+        skifieldList.add(new Skifield("Stratford Mountain Club", R.drawable.smc_icon, "https://www.snow.nz/area/nz/ruapehu/manganui/"));
+        skifieldList.add(new Skifield("Temple Basin", R.drawable.tb_icon, "https://www.snow.nz/area/nz/canterbury/temple-basin/"));
 
         SkifieldAdapter skifieldAdapter = new SkifieldAdapter(skifieldList);
         skifieldsRecyclerView.setAdapter(skifieldAdapter);
